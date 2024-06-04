a8f213b7c0d0c9b6d24036a01dc4fc9baebbb5f3
jinghan-home <jinghan94@126.com>
2024-06-04 21:53:00 +0800

    fix(okx): 资金费率 指标更新历史数据加载3个月

--- src/main/java/org/dromara/northstar/strategy/indicator/OKXFundingRateIndicator.java
+++ src/main/java/org/dromara/northstar/strategy/indicator/OKXFundingRateIndicator.java
@@ -1,8 +1,13 @@
 package org.dromara.northstar.strategy.indicator;
 
 import cn.hutool.core.convert.Convert;
-import cn.hutool.http.Header;
-import cn.hutool.http.HttpRequest;
+import cn.hutool.core.date.DateUnit;
+import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.thread.ThreadUtil;
+import cn.hutool.http.*;
+import cn.hutool.json.JSONArray;
+import cn.hutool.json.JSONObject;
+import cn.hutool.json.JSONUtil;
 import com.alibaba.fastjson2.JSON;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
@@ -13,9 +18,7 @@ import org.dromara.northstar.indicator.model.Num;
 import org.dromara.northstar.strategy.dto.OuYIFundingRateDto;
 
 import java.math.BigDecimal;
-import java.util.Collections;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 /**
  * 欧意 资金费率 0.15%
@@ -26,7 +29,18 @@ public class OKXFundingRateIndicator extends AbstractIndicator implements Indica
 
 
     private static final String RATE_URL = "https://www.okx.com/api/v5/public/funding-rate?instId=";
+
+
+    //    获取最近3个月的历史资金费率
     private static final String HISTORY_URL = "https://www.okx.com/api/v5/public/funding-rate-history";
+    private static final int LIMIT = 100;
+    private static final long THREE_MONTHS_IN_MILLISECONDS = 90L * 24 * 60 * 60 * 1000;
+    /**
+     * 历史费率
+     */
+    private static final TreeMap<Long, Double> historyRateMap = new TreeMap<>();
+    //    是否载入完成
+    private static Boolean isReady = false;
 
 
     /**
@@ -52,6 +66,7 @@ public class OKXFundingRateIndicator extends AbstractIndicator implements Indica
     public OKXFundingRateIndicator(Configuration cfg, String instId) {
         super(cfg);
         this.instId = instId;
+        history(instId);
     }
 
 
@@ -61,34 +76,41 @@ public class OKXFundingRateIndicator extends AbstractIndicator implements Indica
     }
 
     protected Num evaluate(Num num) {
+//        等待载入完成
+        while (!isReady) {
+            ThreadUtil.sleep(1000);
+        }
+//        判断是否有历史数据 floorEntry 获取最接近的 之前的 一个费率
+        Map.Entry<Long, Double> entry = historyRateMap.floorEntry(num.timestamp());
+//        不是最新的一个费率 并且 在4小时 以内
+        if (historyRateMap.lastEntry() != entry || num.timestamp() - entry.getKey() < 1000 * 60 * 4) {
+            BigDecimal rate = BigDecimal.valueOf(entry.getValue());
+            return Num.of(Convert.toDouble(rate.multiply(new BigDecimal(100))), num.timestamp());
+        }
         if (lastDataDTO == null) {
-            return refresh(num);
+            return currentRate(num);
         }
 //        判断下期费率时间
         if (lastDataDTO.getNextFundingTime() < System.currentTimeMillis() ||
 //              提前更新时间 1小时   每5分钟内 不重复更新
                 (lastDataDTO.getNextFundingTime() - System.currentTimeMillis() < advanceTime && System.currentTimeMillis() - lastDataDTO.getTs() > syncTime)) {
-            return refresh(num);
+            return currentRate(num);
         }
         return Num.of(Convert.toDouble(lastFundingRate.multiply(new BigDecimal(100))), num.timestamp());
     }
 
 
     /**
-     * 刷新费率
+     * 获取当前费率
      *
      * @param num
      * @return
      */
-    private Num refresh(Num num) {
+    private Num currentRate(Num num) {
+        //链式构建请求
         try {
-            //链式构建请求
-            if(!instId.contains("SWAP@")){
-                log.error("资金费率请求异常 instId 不正确 {}", instId);
-                return Num.of(0, num.timestamp());
-            }
-            String[] swaps = instId.split("@");
-            String body = HttpRequest.get(RATE_URL + swaps[0])
+            String swap = instId.contains("@") ? instId.split("@")[0] : instId;
+            String body = HttpRequest.get(RATE_URL + swap)
                     .header(Header.USER_AGENT, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36")//头信息，多个头信息多次调用此方法即可
                     .timeout(20000)//超时，毫秒
                     .execute().body();
@@ -98,7 +120,7 @@ public class OKXFundingRateIndicator extends AbstractIndicator implements Indica
                 lastDataDTO = dataDTO;
                 lastFundingRate = dataDTO.getFundingRate();
             }
-        } catch (Exception e) {
+        } catch (HttpException e) {
             log.error("资金费率请求异常", e);
         }
         return Num.of(Convert.toDouble(lastFundingRate.multiply(new BigDecimal(100))), num.timestamp());
@@ -106,40 +128,57 @@ public class OKXFundingRateIndicator extends AbstractIndicator implements Indica
 
 
     /**
-     * 获取历史
-     * @param num
-     * @return
-     * instId    String	是	产品ID ，如 BTC-USD-SWAP 仅适用于永续
-     * before	String	否	请求此时间戳之后（更新的数据）的分页内容，传的值为对应接口的fundingTime
-     * after	String	否	请求此时间戳之前（更旧的数据）的分页内容，传的值为对应接口的fundingTime
-     * limit	String	否	分页返回的结果集数量，最大为100，不填默认返回100条
+     * 获取指定合约ID的最近三个月资金费率
+     *
+     * @param instId 合约ID
      */
-    public static Num history(Num num) {
-        try {
-            //链式构建请求
-            String body = HttpRequest.get(HISTORY_URL)
-                    .form("instId", "BTC-USD-SWAP")
-                    .form("after", num.timestamp())
-                    .form("limit", 1)
-                    .header(Header.USER_AGENT, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36")//头信息，多个头信息多次调用此方法即可
-                    .timeout(20000)//超时，毫秒
-                    .execute().body();
-            OuYIFundingRateDto rateDto = JSON.parseObject(body, OuYIFundingRateDto.class);
-            if (Objects.equals(rateDto.getCode(), "0") && !rateDto.getData().isEmpty()) {
-                OuYIFundingRateDto.DataDTO dataDTO = rateDto.getData().getFirst();
-
+    private void history(String instId) {
+        // 当前时间的时间戳（毫秒）
+        long currentTime = System.currentTimeMillis();
+        // 三个月前的时间戳（毫秒）
+        long threeMonthsAgo = currentTime - THREE_MONTHS_IN_MILLISECONDS;
+        // 如果instId包含"@", 取其"@"之前的部分作为实际合约ID
+        String swap = instId.contains("@") ? instId.split("@")[0] : instId;
+        // 初始请求URL
+        String url = HISTORY_URL + "?instId=" + swap + "&before=" + threeMonthsAgo + "&limit=" + LIMIT;
+        isReady = false;
+        // 循环请求，直到获取到三个月前的数据为止
+        while (true) {
+            String body = null;
+            try {
+                // 发送HTTP GET请求
+                HttpResponse response = HttpRequest.get(url)
+                        .header(Header.USER_AGENT, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36")
+                        .timeout(20000)  // 设置请求超时时间为20000毫秒
+                        .execute();
+                // 获取响应体字符串
+                body = response.body();
+            } catch (Exception e) {
+                ThreadUtil.sleep(1000);
+                continue;
             }
-        } catch (Exception e) {
-            log.error("资金费率请求异常", e);
+            // 解析响应体JSON
+            JSONObject jsonResponse = JSONUtil.parseObj(body);
+            JSONArray data = jsonResponse.getJSONArray("data");
+            // 如果没有数据，则退出循环
+            if (data.isEmpty()) {
+                break;
+            }
+            // 遍历每条数据
+            for (int i = 0; i < data.size(); i++) {
+                JSONObject entry = data.getJSONObject(i);
+                long fundingTime = entry.getLong("fundingTime");  // 资金费时间
+                double fundingRate = entry.getDouble("fundingRate");  // 资金费率
+                // 将资金费时间和资金费率存入TreeMap
+                historyRateMap.put(fundingTime, fundingRate);
+            }
+            // 获取最后一条数据的资金费时间，用于下一次请求分页
+            long lastFundingTime = data.getJSONObject(0).getLong("fundingTime");
+            // 更新URL，添加before参数
+            url = HISTORY_URL + "?instId=" + swap + "&before=" + lastFundingTime + "&limit=" + LIMIT;
+            log.info("载入历史费率完成 " + data.size() + " entries from " + url);
         }
-        return num;
-
-
-
-    }
-
-    public static void main(String[] args) {
-        OKXFundingRateIndicator.history(Num.of(1,1714228276000L));
+        isReady = true;
     }
 
 }
